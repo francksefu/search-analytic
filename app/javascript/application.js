5447b11b9d1060bd0db33fd1a426b0291abfa2dc
francksefu <francksefu1998@gmail.com>
2024-05-26 21:54:54 +0200

    Add table of user and ajax for quering his last search

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -27,11 +27,43 @@ buttonArticles.addEventListener('click', () => {
   tableArticlesUser.style.display = 'none';
   buttonArticles.style.textDecoration = 'underline';
   buttonArticlesUser.style.textDecoration = 'none';
-})
+});
 
 buttonArticlesUser.addEventListener('click', () => {
   tableArticles.style.display = 'none';
   tableArticlesUser.style.display = 'table';
   buttonArticles.style.textDecoration = 'none';
   buttonArticlesUser.style.textDecoration = 'underline';
+});
+
+function takeSearchOfUser(str) {
+  if (str.length == 0) { 
+    document.getElementById("table-user-articles").innerHTML = "";
+    return;
+  }
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("table-user-articles").innerHTML =
+    this.responseText;
+  }
+  xhttp.open("GET", "/user_ips/"+str);
+  xhttp.send();
+}
+
+const buttonViews = document.querySelectorAll('.button-view');
+const tableUserArticles = document.getElementById('table-user-articles');
+const cross = document.getElementById('cross');
+for(let i = 0; i < buttonViews.length; i++) {
+  buttonViews[i].addEventListener('click', () => {
+    tableUserArticles.style.display = 'block';
+    takeSearchOfUser(buttonViews[i].id);
+    document.getElementById('container').style.filter = 'blur(5px)';
+    cross.style.display = 'block';
+  })
+}
+
+cross.addEventListener('click', () => {
+  tableUserArticles.style.display = 'none';
+  document.getElementById('container').style.filter = 'blur(0px)';
+  cross.style.display = 'none';
 })
