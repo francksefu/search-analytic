9b57ed96741572da355497423a70dea9c9c51eff
francksefu <francksefu1998@gmail.com>
2024-05-26 05:26:55 +0200

    Add validation to tables articles and user_ips

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  has_many :user_search_articles
+  has_many :user_ips, through: :user_search_articles
+
+  validates :name, presence: true
 end
--- app/models/user_ip.rb
+++ app/models/user_ip.rb
@@ -1,2 +1,6 @@
 class UserIp < ApplicationRecord
+  has_many :user_search_articles
+  has_many :articles, through: :user_search_articles
+
+  validates :ip, presence: true
 end
