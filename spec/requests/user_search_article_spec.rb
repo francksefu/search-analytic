45d63280711e215d458a81746e9a1cba3f1d96a5
francksefu <francksefu1998@gmail.com>
2024-05-26 18:57:16 +0200

    write test for controller UserSearchArticle

---
+++ spec/requests/user_search_article_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe ' Controller / user_search_article' do
+  
+  let!(:article) do
+    Article.create(name: 'The word help')
+  end
+
+  let!(:user_ip) do
+    UserIp.create(ip: '196.12.3.4')
+  end
+
+  let!(:user_search_article) do
+    UserSearchArticle.create(user_ip: user_ip, article: article)
+  end
+
+  it 'get index for expend' do
+    get "/user_search_articles/new"
+    expect(response.status).to eq(200)
+    expect(response.body).not_to eq nil
+  end
+end
